9ba1ae51ce12eff66cebca5055961b1480cd5973
Mai <jea989@gmail.com>
2023-04-28 20:36:02 -0400

    fixed a little bit

--- drink_calc.py
+++ drink_calc.py
@@ -1,62 +1,49 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+import argparse
+
+liquors = {}
+mixers = {}
 
 
-# done in ML
+## how much mixer and liquor for a simple highball?
+# drink_calc --container-vol 500 --ice-weight 100 --desired-abv 6 --liquor-abv 40
+
+
+## how strong is my drink?
+# drink_calc --ice-weight 100 --mixer-vol 300 --liquor-abv 40 
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--container-vol", dest='container_vol', type=int, help="How many mL is your container")
+ap.add_argument("-m", "--mixer-vol",     dest='mixer_vol',     type=int, help="How many mL of mixer? ")
+ap.add_argument("-l", "--liquor-vol",    dest='liquor_vol',    type=int, help="How many mL liquor]")
+ap.add_argument("-p", "--liquor-abv",    dest='liquor_abv',    type=int, help="How many mL liquor]")
+ap.add_argument("-i", "--ice-weight",    dest='ice_weight',    type=int, default=0, help="How many grams of ice?")
+ap.add_argument("-a", "--final-abv",     dest='final_abv',     type=int, help="How much alcohol by volume should the result contain?")
+a = vars(ap.parse_args())
+
+a["ice_vol"] = a["ice_weight"] / 0.9168
+
+ml_to_floz = lambda ml : ml * .0338
+
+#TODO: ice melting
+
+
+if a["final_abv"] and not a["mixer_vol"] and not a["liquor_vol"]:
+  a["delution_ratio"] = a["liquor_abv"] / a["final_abv"]
+  a["liquor_vol"] = a["container_vol"] / a["delution_ratio"]
+  a["mixer_vol"] = a["container_vol"] - a["liquor_vol"] - a["ice_vol"]
+  
 
 
 
-liquor_percent = 45
 
 
-
-#base_volume = (500 + 750)
-bottle_volume = 450
-desired_final_percent = 10
-#liquor_volume = 1
-
-
-#liquor_percent = 40
-#bottle_volume = 355
-#base_volume = 310
-# above equals 45 which is ~ 1/5 floz or 1 shot, which should be equal abv to 1 beer at 5%
-# used as a test to prove the equation
-
-
-
-def find_liqor_volume():
-  for i in range(0, 3000):
-    test_percent = (i * liquor_percent) / (base_volume + i)
-    print(i, test_percent)
-    if test_percent >= desired_final_percent:
-      print(i, test_percent)
-      break
-
-
-def find_base_volume():
-  total_parts = liquor_percent / desired_final_percent  
-  base_parts = total_parts - 1
-  base_vol = base_parts * liquor_volume
-  base_vol_oz = base_vol * .0338
-  print(base_vol)
-  print(base_vol_oz)
-
-def find_percent():
-  alc_percent = (liquor_percent * liquor_volume) / (base_volume + liquor_volume)
-  print(alc_percent)
-
-def find_base_and_liquor_volume_bottle_volume():
-  # neet bottle_volume and desired_final_percent and liquor_percent
-
-  # first determine ratio to dilute liquor to desired percent
-  # 43 / 5 = 8.6 or 8.6:1
-  delution_ratio = liquor_percent / desired_final_percent
-  liquor_volume = bottle_volume / delution_ratio
-  base_volume = bottle_volume - liquor_volume 
-  print(liquor_volume, base_volume) 
-
-#find_base_volume()
-#find_liqor_volume()
-#find_percent()
-find_base_and_liquor_volume_bottle_volume()
-
+print(f'{a["container_vol"]=}')
+print(f'{a["mixer_vol"]=}')
+print(f'{a["liquor_vol"]=}')
+print(f'{a["liquor_abv"]=}')
+print(f'{a["ice_weight"]=}')
+print(f'{a["ice_vol"]=}')
+print(f'{a["final_abv"]=}')
 
