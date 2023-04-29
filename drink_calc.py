65558d30be94ce434f232d75ef0182458177981d
Mai <jea989@gmail.com>
2023-04-29 12:40:52 -0400

    readme & cleanup & ice_abv

--- drink_calc.py
+++ drink_calc.py
@@ -1,42 +1,35 @@
 #!/usr/bin/env python3
 
-import argparse
-
-liquors = {}
-mixers = {}
-
-
 ## how much mixer and liquor for a simple highball?
 # drink_calc --container-vol 500 --ice-weight 100 --desired-abv 6 --liquor-abv 40
-
 
 ## how strong is my drink?
 # drink_calc --ice-weight 100 --mixer-vol 300 --liquor-abv 40 
 
+#TODO: multiple liquors and mixers
+# example: cosmopolitan: 40ml vodka 40%, 15ml triplesec 24%, 30ml cranberry juice, 15ml lime juice
+
+import argparse
+
 ap = argparse.ArgumentParser()
-ap.add_argument("-c", "--container-vol", dest='container_vol', type=int, help="How many mL is your container")
-ap.add_argument("-m", "--mixer-vol",     dest='mixer_vol',     type=int, help="How many mL of mixer? ")
-ap.add_argument("-l", "--liquor-vol",    dest='liquor_vol',    type=int, help="How many mL liquor]")
-ap.add_argument("-p", "--liquor-abv",    dest='liquor_abv',    type=int, help="How many mL liquor]")
+ap.add_argument("-c", "--container-vol", dest='container_vol', type=int,            help="How many mL is your container")
+ap.add_argument("-m", "--mixer-vol",     dest='mixer_vol',     type=int,            help="How many mL of mixer? ")
+ap.add_argument("-l", "--liquor-vol",    dest='liquor_vol',    type=int,            help="How many mL liquor]")
+ap.add_argument("-p", "--liquor-abv",    dest='liquor_abv',    type=int,            help="What is the alcohol percentage by volume?]")
 ap.add_argument("-i", "--ice-weight",    dest='ice_weight',    type=int, default=0, help="How many grams of ice?")
-ap.add_argument("-a", "--final-abv",     dest='final_abv',     type=int, help="How much alcohol by volume should the result contain?")
+ap.add_argument("-a", "--final-abv",     dest='final_abv',     type=int,            help="How much alcohol by volume should the result contain?")
 a = vars(ap.parse_args())
 
 a["ice_vol"] = a["ice_weight"] / 0.9168
 
 ml_to_floz = lambda ml : ml * .0338
 
-#TODO: ice melting
-
 
 if a["final_abv"] and not a["mixer_vol"] and not a["liquor_vol"]:
   a["delution_ratio"] = a["liquor_abv"] / a["final_abv"]
   a["liquor_vol"] = a["container_vol"] / a["delution_ratio"]
   a["mixer_vol"] = a["container_vol"] - a["liquor_vol"] - a["ice_vol"]
-  
-
-
-
+  a["ice_abv"] = (a["liquor_vol"] * a["liquor_abv"]) / (a["mixer_vol"] + a["liquor_vol"])
 
 
 print(f'{a["container_vol"]=}')
@@ -45,5 +38,6 @@ print(f'{a["liquor_vol"]=}')
 print(f'{a["liquor_abv"]=}')
 print(f'{a["ice_weight"]=}')
 print(f'{a["ice_vol"]=}')
+print(f'{a["ice_abv"]=}')
 print(f'{a["final_abv"]=}')
 
---
+++ readme.md
@@ -0,0 +1,24 @@
+## drink_calc.py
+
+just a commandline mixed drink calculator that can help with preparing recipes & ratios
+
+
+#### TODO:
+a lot of things... more features will be added when i decide i need them.
+
+got any ideas? open an issue
+
+#### Example: a Highball
+
+```
+[mai@Mvg drink_calc]$ ./drink_calc.py --container-vol 500 --ice-weight 68 --final-abv 10 --liquor-abv 40
+a["container_vol"]=500
+a["mixer_vol"]=300.82897033158815
+a["liquor_vol"]=125.0
+a["liquor_abv"]=40
+a["ice_weight"]=68
+a["ice_vol"]=74.17102966841188
+a["ice_abv"]=11.741803278688524
+a["final_abv"]=10
+
+```
